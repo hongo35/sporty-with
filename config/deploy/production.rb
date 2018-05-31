ddccff7a27e8f4d93298db9a77ddb34a202fbc0d
Yutaka Hongo <s.style3.5@gmail.com>
2018-06-01 01:57:57 +0900

    [update] session周り修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,9 @@ set :rbenv_type, :system
 set :rbenv_ruby, '2.4.1'
 set :rbenv_custom_path, '/usr/local/rbenv'
 
-server 'sakura', user: 'hongo35', role: %w{app db web}
+# server 'sakura', user: 'hongo35', role: %w{app db web}
+server 'sakura', user: 'hongo35', role: %w{app web}
 set :stage, :production
 set :assets_roles, [:web, :app]
 role :app, %w(sakura)
-role :db, %w(sakura), no_release: true
+# role :db, %w(sakura), no_release: true
